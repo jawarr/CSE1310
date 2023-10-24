dd69622f8a46d61f383f94badfa0a6bf6d392392
Jacob <your_email@real_email.edu>
2023-10-24 12:14:48 -0500

    deviation as a percentage

--- week9/sixMillionDiceRolls.c
+++ week9/sixMillionDiceRolls.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
+#include <math.h>
 
 int main (void) {
     
@@ -36,12 +37,14 @@ int main (void) {
         }
     }
     int devArr[6] = {0};
+    float perArr[6] = {0};
     for (int i = 0; i < 6; i++) {
         devArr[i] = freqArr[i] - 1000000;
+        perArr[i] = fabs(devArr[i] * 0.0001);
     }
     
+    
     printf("Frequency of each roll:\n1:%15d\n2:%15d\n3:%15d\n4:%15d\n5:%15d\n6:%15d\n", freqArr[0], freqArr[1], freqArr[2], freqArr[3], freqArr[4], freqArr[5]);
-    printf("Deviation from the mean:\n1:%15d\n2:%15d\n3:%15d\n4:%15d\n5:%15d\n6:%15d\n", devArr[0], devArr[1], devArr[2], devArr[3], devArr[4], devArr[5]);
-    // printf("Deviation as a percentage:\n1:%15f\n2:%15f\n3:%15f\n4:%15f\n5:%15f\n6:%15f\n", freqArr[0]/1000000.0, freqArr[1]/1000000.0, freqArr[2]/1000000.0, freqArr[3]/1000000.0, freqArr[4]/1000000.0, freqArr[5]/1000000.0);
+    printf("Deviation from the mean:\n1:%15d%15.4f%%\n2:%15d%15.4f%%\n3:%15d%15.4f%%\n4:%15d%15.4f%%\n5:%15d%15.4f%%\n4:%15d%15.4f%%\n", devArr[0], perArr[0], devArr[1], perArr[1], devArr[2], perArr[2], devArr[3], perArr[3], devArr[4], perArr[4], devArr[5], perArr[5]);
     return 0;
 }
