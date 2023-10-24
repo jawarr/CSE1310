baf94769c9aaf1feeda339b305c061bae19b1b82
Jacob <your_email@real_email.edu>
2023-10-24 11:56:31 -0500

    switched to storing values in an array

--- week9/sixMillionDiceRolls.c
+++ week9/sixMillionDiceRolls.c
@@ -3,41 +3,45 @@
 #include <time.h>
 
 int main (void) {
+    
     srand(time(NULL));
-    int i = 0, 
-        one = 0, 
-        two = 0, 
-        three = 0, 
-        four = 0, 
-        five = 0, 
-        six = 0;
-    for (i; i < 6000000; i++) {
-        int n = 1 + rand() % 6;
+    
+    int freqArr[6] = {0};
+    
+    for (int i = 0; i < 6000000; i++) {
+        int n = rand() % 6;
         switch (n) {
+            case 0:
+                freqArr[n]++;
+                break;
             case 1:
-                one++;
+                freqArr[n]++;
                 break;
             case 2:
-                two++;
+                freqArr[n]++;
                 break;
             case 3:
-                three++;
+                freqArr[n]++;
                 break;
             case 4:
-                four++;
+                freqArr[n]++;
                 break;
             case 5:
-                five++;
-                break;
-            case 6:
-                six++;
+                freqArr[n]++;
                 break;  
             default:
+                printf("Something bad happened");
+                return 0;
                 break;
         }
     }
-    printf("Frequency of each roll:\n1:%15d\n2:%15d\n3:%15d\n4:%15d\n5:%15d\n6:%15d\n", one, two, three, four, five, six);
-    printf("Deviation from the mean:\n1:%15d\n2:%15d\n3:%15d\n4:%15d\n5:%15d\n6:%15d\n", one-1000000, two-1000000, three-1000000, four-1000000, five-1000000, six-1000000);
-    printf("Deviation as a percentage:\n1:%15f\n2:%15f\n3:%15f\n4:%15f\n5:%15f\n6:%15f\n", one/1000000.0, two/1000000.0, three/1000000.0, four/1000000.0, five/1000000.0, six/1000000.0);
+    int devArr[6] = {0};
+    for (int i = 0; i < 6; i++) {
+        devArr[i] = freqArr[i] - 1000000;
+    }
+    
+    printf("Frequency of each roll:\n1:%15d\n2:%15d\n3:%15d\n4:%15d\n5:%15d\n6:%15d\n", freqArr[0], freqArr[1], freqArr[2], freqArr[3], freqArr[4], freqArr[5]);
+    printf("Deviation from the mean:\n1:%15d\n2:%15d\n3:%15d\n4:%15d\n5:%15d\n6:%15d\n", devArr[0], devArr[1], devArr[2], devArr[3], devArr[4], devArr[5]);
+    // printf("Deviation as a percentage:\n1:%15f\n2:%15f\n3:%15f\n4:%15f\n5:%15f\n6:%15f\n", freqArr[0]/1000000.0, freqArr[1]/1000000.0, freqArr[2]/1000000.0, freqArr[3]/1000000.0, freqArr[4]/1000000.0, freqArr[5]/1000000.0);
     return 0;
 }
