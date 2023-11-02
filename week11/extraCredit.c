6cb858b6d3a364f7cc71f2e5dfb4fb5f6ea108b4
Jacob <your_email@real_email.edu>
2023-11-01 20:27:43 -0500

    row 1 and row 2 swap

--- week11/extraCredit.c
+++ week11/extraCredit.c
@@ -1,10 +1,13 @@
 // Write a C program that receives a 2 dimensional array of n x m dimensions.
-// Your program should print the row exchanged 2 dimensional arrays below:
+// Your program should print the row-exchanged 2 dimensional arrays below:
 //                  R1 <----> R2
 //                  R2 <----> R3
 //                  R3 <----> R1
 
 #include <stdio.h>
+
+void copy2DimensionalArray(int rows, int columns, int source[rows][columns], int target[rows][columns]);
+void print2DimensionalArray(int rows, int columns, int source[rows][columns]);
 
 int main (void) {
     
@@ -27,20 +30,65 @@ int main (void) {
         {
             scanf("%d", &array[i][j]);
         }
-        
-        
     }
-    printf("\n\n");
+    printf("\n");
+    
     //Print Array
+    printf("Initial Array:\n");
+    print2DimensionalArray(rows, columns, array);
+    
+    //Create a copy of rows 1 thru 3
+    int r1[columns], r2[columns], r3[columns];
+    
+    for (int i = 0; i < columns; i++)
+    {
+        r1[i] = array[0][i];
+        r2[i] = array[1][i];
+        r3[i] = array[2][i];
+    }
+    
+    //R1 <----> R2
+    int r1r2swap[rows][columns];
+
+    copy2DimensionalArray(rows, columns, array, r1r2swap);
+
+    for (int i = 0; i < rows; i++)
+    {   
+        for (int j = 0; j < columns; j++)
+        {
+            {
+                r1r2swap[0][j] = r2[j];
+                r1r2swap[1][j] = r1[j];
+            }
+        }
+    }
+    
+
+    printf("Row 1 exchanged with Row 2:\n");
+    print2DimensionalArray(rows, columns, r1r2swap);
+    
+    return 0;
+}
+
+void copy2DimensionalArray(int rows, int columns, int source[rows][columns], int target[rows][columns])
+{
+    for (int i = 0; i < rows; i++) 
+    {
+        for (int j = 0; j < columns; j++) 
+        {
+            target[i][j] = source[i][j];
+        }
+   }
+}
+
+void print2DimensionalArray(int rows, int columns, int source[rows][columns])
+{
     for (int i = 0; i < rows; i++)
     {
         for (int j = 0; j < columns; j++)
         {
-            printf("%10.d", array[i][j]);
+            printf("%10.d", source[i][j]);
         }
         printf("\n\n");
     }
-    
-
-    return 0;
 }
