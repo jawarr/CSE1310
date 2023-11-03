2ad7917429fc45801e147ae11abf7c1cbaecce90
Jacob <your_email@real_email.edu>
2023-11-03 10:23:22 -0500

    question 7 complete

--- week11/hw3.c
+++ week11/hw3.c
@@ -2,22 +2,12 @@
 #include <stdlib.h>
 #include <time.h>
 
-char gradeEval (float numGrade) 
-{
-    if (numGrade >= 90.0) return 'A';
-    else if (numGrade >= 80.0) return 'B';
-    else if (numGrade >= 70.0) return 'C';
-    else if (numGrade >= 60.0) return 'D';
-    else if (numGrade >= 40.0) return 'E';
-    else if (numGrade >= 0.0) return 'F';
-    else return 'X';
-}
-
-void print_average (int a, int b, int c)
-{
-    float average = (a + b + c) / 3.0;
-    printf("The average of %d, %d, and %d is %.3f\n", a, b, c, average);
-}
+char gradeEval (float numGrade);
+void print_average (int a, int b, int c);
+int makeSquare (int num);
+void makeSquareDirectly (int array[],int i, int size);
+int arraySum (int array[], int size);
+void isOdd (int num);
 
 int main (void) 
 { 
@@ -65,26 +55,109 @@ int main (void)
     // scanf("%d%d%d", &a, &b, &c);
     // print_average(a, b, c);
 
-    printf("\n---------------Question 4---------------\n");
-    int intArray[4], index1, index2;
-    printf("Enter four numbers for the array: ");
-    scanf("%d%d%d%d", &intArray[0], &intArray[1], &intArray[2], &intArray[3]);
-    printf("\nInital array: %10.d%10.d%10.d%10.d\n", intArray[0], intArray[1], intArray[2], intArray[3]);
+    // printf("\n---------------Question 4---------------\n");
+    // int intArray[4], index1, index2;
+    // printf("Enter four numbers for the array: ");
+    // scanf("%d%d%d%d", &intArray[0], &intArray[1], &intArray[2], &intArray[3]);
+    // printf("\nInital array:\n%10.d%10.d%10.d%10.d\n", intArray[0], intArray[1], intArray[2], intArray[3]);
     
-    printf("\nEnter a index (0-3) to print: ");
-    scanf("%d", &index1);
-    printf("\nValue currently at index %d: %d\n", index1, intArray[index1]);
+    // printf("\nEnter a index (0-3) to print: ");
+    // scanf("%d", &index1);
+    // printf("\nValue currently at index %d: %d\n", index1, intArray[index1]);
     
-    printf("\nEnter two indices to swap: ");
-    scanf("%d%d", &index1, &index2);
+    // printf("\nEnter two indices to swap: ");
+    // scanf("%d%d", &index1, &index2);
     
-    int a, b;
-    a = intArray[index1];
-    b = intArray[index2];
-    intArray[index1] = b;
-    intArray[index2] = a;
-    printf("\nUpdated array: %10.d%10.d%10.d%10.d\n", intArray[0], intArray[1], intArray[2], intArray[3]);
+    // int a, b;
+    // a = intArray[index1];
+    // b = intArray[index2];
+    // intArray[index1] = b;
+    // intArray[index2] = a;
+    // printf("\nUpdated array:\n%10.d%10.d%10.d%10.d\n", intArray[0], intArray[1], intArray[2], intArray[3]);
+
+    // printf("\n---------------Question 5---------------\n");
+    // int squareArray[4], squareIndex;
+    
+    // printf("Enter four numbers for the array: ");
+    // scanf("%d%d%d%d", &squareArray[0], &squareArray[1], &squareArray[2], &squareArray[3]);
+    // printf("\nInital array:\n%10.d%10.d%10.d%10.d\n", squareArray[0], squareArray[1], squareArray[2], squareArray[3]);
+
+    // printf("\nEnter a index (0-3) to square: ");
+    // scanf("%d", &squareIndex);
+    // printf("\nValue currently at index %d: %d\n", squareIndex, squareArray[squareIndex]);
+    
+    // squareArray[squareIndex] = makeSquare(squareArray[squareIndex]);
+    // printf("\nUpdated array:\n%10.d%10.d%10.d%10.d\n", squareArray[0], squareArray[1], squareArray[2], squareArray[3]);
+
+    // printf("\n---------------Question 6---------------\n");
+    // int integerArray[4], squareIndexQ6;
+    // printf("Enter four numbers for the array: ");
+    // scanf("%d%d%d%d", &integerArray[0], &integerArray[1], &integerArray[2], &integerArray[3]);
+    // printf("\nInital array:\n%10.d%10.d%10.d%10.d\n", integerArray[0], integerArray[1], integerArray[2], integerArray[3]);
+    // printf("\nEnter a index (0-3) to square: ");
+    // scanf("%d", &squareIndexQ6);
+    // makeSquareDirectly(integerArray, squareIndexQ6, 4);
+    
+    // printf("\n---------------Question 7---------------\n");
+    // int sumArray[5];
+    // printf("Enter five numbers for the array: ");
+    // scanf("%d%d%d%d%d", &sumArray[0], &sumArray[1], &sumArray[2], &sumArray[3], &sumArray[4]);
+    // printf("\nInital array:\n%10.d%10.d%10.d%10.d%10.d\n", sumArray[0], sumArray[1], sumArray[2], sumArray[3], sumArray[4]);
+    // printf("The sum of the numbers in the array is ");
+    // isOdd(arraySum(sumArray, 5));
+    // printf("\n");
+
+    printf("\n---------------Question 8---------------\n");
 
 
     return 0;
+}
+
+char gradeEval (float numGrade) 
+{
+    if (numGrade >= 90.0) return 'A';
+    else if (numGrade >= 80.0) return 'B';
+    else if (numGrade >= 70.0) return 'C';
+    else if (numGrade >= 60.0) return 'D';
+    else if (numGrade >= 40.0) return 'E';
+    else if (numGrade >= 0.0) return 'F';
+    else return 'X';
+}
+
+void print_average (int a, int b, int c)
+{
+    float average = (a + b + c) / 3.0;
+    printf("The average of %d, %d, and %d is %.3f\n", a, b, c, average);
+}
+
+int makeSquare (int num)
+{
+    return num*num;
+}
+
+void makeSquareDirectly (int array[], int i, int size)
+{
+    array[i] = array[i] * array[i];
+    for (int j = 0; j < size; j++)
+    {
+        printf("%10.d", array[j]);
+    }
+    printf("\n");
+
+}
+
+int arraySum (int array[], int size) 
+{
+    int sum = 0;
+    for (int i = 0; i < size; i++)
+    {
+        sum += array[i];
+    }
+    return sum;
+}
+
+void isOdd (int num)
+{
+    if ((num % 2) == 1) printf("odd");
+    else printf("even");
 }
