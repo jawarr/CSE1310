fada3bd6b8b28ab2f833af82c3b134087675fb51
Jacob <your_email@real_email.edu>
2023-11-07 10:30:03 -0600

    extra credit 2 complete

---
+++ week12/extraCredit2.c
@@ -0,0 +1,48 @@
+#include <stdio.h>
+
+void populateAccendingArray(int size, int source[size]); 
+void printArray(int size, int source[size]);
+
+int main(void) 
+{
+    int num;
+    printf("Enter the number of arrays to print (max of 10): ");
+    scanf("%d", &num);
+    if (num > 0 && num <= 10)
+    {
+        int array[num];
+    
+        for (int i = 0; i < num; i++)
+        {
+            populateAccendingArray(num, array);
+        }
+    
+        int size = num;
+        for (int i = 0; i < num; i++)
+        {
+            printArray(size, array);
+            size--;
+        }
+    } else printf("Please enter a valid number (1-10)\n");
+    
+    return 0;
+}
+
+void populateAccendingArray(int size, int source[size]) 
+{
+    for (int i = 0; i < size; i++)
+    {
+        source[i] = i + 1;
+    }
+}
+
+void printArray(int size, int source[size])
+{
+    printf("[");
+    for (int i = 0; i < size - 1; i++)
+    {
+        printf("%d, ", source[i]);
+    }
+    printf("%d", source[size - 1]);
+    printf("]\n");
+}
