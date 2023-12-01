96926ce7c5b10d083ed87c9e2ac4ce23b6b15e13
Jacob Warren <jacobiwarren@gmail.com>
2023-11-30 23:31:06 -0600

    question 3 complete

--- week15/hw4.c
+++ week15/hw4.c
@@ -4,6 +4,7 @@
 
 int dotProduct(int size, int vector1[size], int vector2[size]);
 void print4x4Array(int source[4][4]);
+void print2DimensionalArray(int rows, int columns, int source[rows][columns]);
 
 int main(void) 
 {
@@ -31,140 +32,191 @@ int main(void)
     // //Call dotProduct func and print output
     // printf("The dot product of the two vectors is %d.\n", dotProduct(size, vector1, vector2));
     
-    printf("\n---------------Question 2---------------\n");
+    // printf("\n---------------Question 2---------------\n");
     
-    //Array 1
-    int array1[4][4];
-    srand(time(NULL));
-    for (int i = 0; i < 4; i++)
-    {
-        switch (i)
-        {
-        case 0:
-            for (int j = 0; j < 4; j++)
-            {
-                if (j == 0) array1[i][j] = rand() % 9 + 1;
-                else array1[i][j] = 0;
-            }
-            break;
-
-        case 1:
-            for (int j = 0; j < 4; j++)
-            {
-                if (j <= 1) array1[i][j] = rand() % 9 + 1;
-                else array1[i][j] = 0;
-            }
-            break;
-        
-        case 2:
-            for (int j = 0; j < 4; j++)
-            {
-                if (j <= 2) array1[i][j] = rand() % 9 + 1;
-                else array1[i][j] = 0;
-            }
-            break;
-
-        case 3:
-            for (int j = 0; j < 4; j++)
-            {
-                array1[i][j] = rand() % 9 + 1;
-            }
-            break;
-        default:
-            break;
-        }
-        
-    }
-    print4x4Array(array1);
+    // //Array 1
+    // int array1[4][4];
+    // srand(time(NULL));
+    // printf("i)\n");
     
-    //Array 2
-    int array2[4][4];
-    for (int i = 0; i < 4; i++)
+    // for (int i = 0; i < 4; i++)
+    // {
+    //     switch (i)
+    //     {
+    //     case 0:
+    //         for (int j = 0; j < 4; j++)
+    //         {
+    //             if (j == 0) array1[i][j] = rand() % 9 + 1;
+    //             else array1[i][j] = 0;
+    //         }
+    //         break;
+
+    //     case 1:
+    //         for (int j = 0; j < 4; j++)
+    //         {
+    //             if (j <= 1) array1[i][j] = rand() % 9 + 1;
+    //             else array1[i][j] = 0;
+    //         }
+    //         break;
+        
+    //     case 2:
+    //         for (int j = 0; j < 4; j++)
+    //         {
+    //             if (j <= 2) array1[i][j] = rand() % 9 + 1;
+    //             else array1[i][j] = 0;
+    //         }
+    //         break;
+
+    //     case 3:
+    //         for (int j = 0; j < 4; j++)
+    //         {
+    //             array1[i][j] = rand() % 9 + 1;
+    //         }
+    //         break;
+    //     default:
+    //         break;
+    //     }
+        
+    // }
+    // print4x4Array(array1);
+    
+    // //Array 2
+    // int array2[4][4];
+    // printf("ii)\n");
+    
+    // for (int i = 0; i < 4; i++)
+    // {
+    //     switch (i)
+    //     {
+    //     case 0:
+    //         for (int j = 0; j < 4; j++)
+    //         {
+    //             array2[i][j] = rand() % 9 + 1;
+    //         }
+    //         break;
+
+    //     case 1:
+    //         for (int j = 0; j < 4; j++)
+    //         {
+    //             if (j >= 1) array2[i][j] = rand() % 9 + 1;
+    //             else array2[i][j] = 0;
+    //         }
+    //         break;
+        
+    //     case 2:
+    //         for (int j = 0; j < 4; j++)
+    //         {
+    //             if (j >= 2) array2[i][j] = rand() % 9 + 1;
+    //             else array2[i][j] = 0;
+    //         }
+    //         break;
+
+    //     case 3:
+    //         for (int j = 0; j < 4; j++)
+    //         {
+    //             if (j >= 3) array2[i][j] = rand() % 9 + 1;
+    //             else array2[i][j] = 0;
+    //         }
+    //         break;
+    //     default:
+    //         break;
+    //     }
+        
+    // }
+    // print4x4Array(array2);
+
+    // //Array 3
+    // int array3[4][4];
+    // printf("iii)\n");
+    
+    // for (int i = 0; i < 4; i++)
+    // {
+    //     switch (i)
+    //     {
+    //     case 0:
+    //         for (int j = 0; j < 4; j++)
+    //         {
+    //             if (j == 0) array3[i][j] = rand() % 9 + 1;
+    //             else array3[i][j] = 0;
+    //         }
+    //         break;
+
+    //     case 1:
+    //         for (int j = 0; j < 4; j++)
+    //         {
+    //             if (j == 1) array3[i][j] = rand() % 9 + 1;
+    //             else array3[i][j] = 0;
+    //         }
+    //         break;
+        
+    //     case 2:
+    //         for (int j = 0; j < 4; j++)
+    //         {
+    //             if (j == 2) array3[i][j] = rand() % 9 + 1;
+    //             else array3[i][j] = 0;
+    //         }
+    //         break;
+
+    //     case 3:
+    //         for (int j = 0; j < 4; j++)
+    //         {
+    //             if (j == 3) array3[i][j] = rand() % 9 + 1;
+    //             else array3[i][j] = 0;
+    //         }
+    //         break;
+    //     default:
+    //         break;
+    //     }
+        
+    // }
+    // print4x4Array(array3);
+    
+    printf("\n---------------Question 3---------------\n");
+
+    //Initialize arrays
+    int rows, columns;
+
+    printf("Enter the number of rows and columns for both arrays: ");
+    scanf("%d%d", &rows, &columns);
+    int array1[rows][columns];
+    int array2[rows][columns];
+    int sumArray[rows][columns];
+
+    //Populate 2 arrays
+    for (int i = 0; i < rows; i++)
     {
-        switch (i)
+        printf("Enter the values for array 1 row %d: ", i+1);
+        
+        for (int j = 0; j < columns; j++)
         {
-        case 0:
-            for (int j = 0; j < 4; j++)
-            {
-                array2[i][j] = rand() % 9 + 1;
-            }
-            break;
-
-        case 1:
-            for (int j = 0; j < 4; j++)
-            {
-                if (j >= 1) array2[i][j] = rand() % 9 + 1;
-                else array2[i][j] = 0;
-            }
-            break;
-        
-        case 2:
-            for (int j = 0; j < 4; j++)
-            {
-                if (j >= 2) array2[i][j] = rand() % 9 + 1;
-                else array2[i][j] = 0;
-            }
-            break;
-
-        case 3:
-            for (int j = 0; j < 4; j++)
-            {
-                if (j >= 3) array2[i][j] = rand() % 9 + 1;
-                else array2[i][j] = 0;
-            }
-            break;
-        default:
-            break;
+            scanf("%d", &array1[i][j]);
         }
-        
     }
-    print4x4Array(array2);
-
-    //Array 3
-    int array3[4][4];
-    for (int i = 0; i < 4; i++)
+    
+    for (int i = 0; i < rows; i++)
     {
-        switch (i)
+        printf("Enter the values for array 2 row %d: ", i+1);
+        
+        for (int j = 0; j < columns; j++)
         {
-        case 0:
-            for (int j = 0; j < 4; j++)
-            {
-                if (j == 0) array3[i][j] = rand() % 9 + 1;
-                else array3[i][j] = 0;
-            }
-            break;
-
-        case 1:
-            for (int j = 0; j < 4; j++)
-            {
-                if (j == 1) array3[i][j] = rand() % 9 + 1;
-                else array3[i][j] = 0;
-            }
-            break;
-        
-        case 2:
-            for (int j = 0; j < 4; j++)
-            {
-                if (j == 2) array3[i][j] = rand() % 9 + 1;
-                else array3[i][j] = 0;
-            }
-            break;
-
-        case 3:
-            for (int j = 0; j < 4; j++)
-            {
-                if (j == 3) array3[i][j] = rand() % 9 + 1;
-                else array3[i][j] = 0;
-            }
-            break;
-        default:
-            break;
+            scanf("%d", &array2[i][j]);
         }
-        
     }
-    print4x4Array(array3);
 
+    //Add 2 arrays
+    for (int i = 0; i < rows; i++)
+    {   
+        for (int j = 0; j < columns; j++)
+        {
+            sumArray[i][j] = array1[i][j] + array2[i][j];
+        }
+    }
+    printf("\n");
 
+    //Print sum array
+    printf("Sum of both arrays:\n");
+    print2DimensionalArray(rows, columns, sumArray);
+    
     return 0;
 }
 
@@ -191,4 +243,16 @@ void print4x4Array(int source[4][4])
         printf("\n");
     }
     printf("\n");
+}
+
+void print2DimensionalArray(int rows, int columns, int source[rows][columns])
+{
+    for (int i = 0; i < rows; i++)
+    {
+        for (int j = 0; j < columns; j++)
+        {
+            printf("%10.d", source[i][j]);
+        }
+        printf("\n\n");
+    }
 }
